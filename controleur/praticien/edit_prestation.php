5ed0d1c9eb604c839b608196816dee3210afdb52
Clement B <clement.bollore@gmail.com>
2022-05-20 14:21:35 +0200

    ajout creation prestation  dans comptepro

--- controleur/praticien/edit_prestation.php
+++ controleur/praticien/edit_prestation.php
@@ -1,61 +1,20 @@
 <?php
-require  VUE_DIR.'edit_prestation.html'
-?>
+include   MODELE_DIR.'edit_prestation.inc.php';
 
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Page de traitement</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <p>Dans le formulaire précédent, vous avez fourni les
-        informations suivantes :</p>
-        
-        <?php
-            echo 'Acte : '.$_POST["acte"].'<br>';
-            echo 'Durée : ' .$_POST["duree"].'<br>';
-            echo 'Tarif : ' .$_POST["tarif"].'<br>';
-            addPrestation();
-        ?>
+if (!empty($_POST)) {
+    echo 'Nouvelle prestation crée avec succès';
+    header('Location: ?action=comptepro');
+    exit;
+}else{
+    echo "Un problème est survenu lors de la création de la prestation,";
 
-        <?php
+}
 
-           // die(MODELE_DIR.'bd.inc.php');
 
-            
-            function addPrestation(){
-            $acte=$_POST["acte"];
-            $duree=$_POST["duree"];
-            $tarif=$_POST["tarif"];
-                try {
-                    $cnx = getPdo();
-                    $req = $cnx -> prepare("insert into prestation (id_praticien,acte, duree,tarif) values('3', '$acte','$duree', '$tarif')");
-                    $req -> execute();
-                } catch (PDOException $e) {
-                    print "Erreur !: " . $e->getMessage();
-                    die();
-                }
-            }
-        ?>
 
-        <?php
-            // function getPdo(){
-            //     $login = 'dev';
-            //     $mdp = 'SuperUser@sio56';
-            //     $bd = 'gr1';
-            //     $serveur = '172.16.120.2';
-            //     $port = "3307";
 
-            //     try {
-            //         $inst = new PDO("mysql:host=$serveur;port=$port;dbname=$bd", $login, $mdp,);
-            //         $inst->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            //         return $inst;
-            //     } catch (PDOException $e) {
-            //         die('Erreur : ' . $e->getMessage());
-            //     }
-            // }
-        ?>
-    </body>
-</html>
+
+
+
+
